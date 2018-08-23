df69841e723478ec687c5694fc63e6c6797c0f4b
Dwayne Coussement <Dwayne@intivoto.com>
2018-08-23 15:12:47 +0200

    Introduce the keyboardlistener; to be able to catch events.

--- NYSKeyboardHelper/Classes/NYSKeyboardHelper.swift
+++ NYSKeyboardHelper/Classes/NYSKeyboardHelper.swift
@@ -9,80 +9,42 @@
 import UIKit
 
 public class NYSKeyboardHelper: NSLayoutConstraint {
-    @IBInspectable public var extraIndent: CGFloat = 0.0
-    private var lastNotification: Notification? = nil
-    
-    override init() {
-        super.init()
-        setup()
-    }
-    
-    public override func awakeFromNib() {
-        super.awakeFromNib()
-        setup()
-    }
-    
-    private func setup() {
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidChangeVisible(notification:)), name: .UIKeyboardWillHide, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidChangeVisible(notification:)), name: .UIKeyboardWillShow, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidChangeVisible(notification:)), name: .UIKeyboardDidShow, object: nil)
-    }
-    
-    deinit {
-        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
-        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
-        NotificationCenter.default.removeObserver(self, name: .UIKeyboardDidShow, object: nil)
-    }
-    
+	private var keyboardListener: NYSKeyboardListener?
+	@IBInspectable public var extraIndent: CGFloat = 0.0 {
+		didSet {
+			keyboardListener?.extraIndent = extraIndent
+		}
+	}
+	override init() {
+		super.init()
+		setup()
+	}
+
+	public override func awakeFromNib() {
+		super.awakeFromNib()
+		setup()
+	}
+
+	private func setup() {
+		keyboardListener = NYSKeyboardListener(constraint: self)
+		keyboardListener?.keyboardUpdate = { [weak self] (animationOptions, animationDuration, keyboardHeight) in
+			guard let `self` = self,
+				let firstView = self.firstItem as? UIView else { return }
+			let options = UIViewAnimationOptions(rawValue: animationOptions.rawValue | UIViewAnimationOptions.layoutSubviews.rawValue)
+
+			UIView.animate(withDuration: animationDuration, delay: 0.0, options: options, animations: {
+				self.constant = keyboardHeight
+				firstView.layoutIfNeeded()
+			}, completion: nil)
+		}
+	}
+
     /// This can be called to update the constraint if the view moves (without keyboard appearing/disappearing)
     /// Should be used in a PopoverViewController -> viewWillLayoutSubviews
     public func update() {
-        if let notification = lastNotification {
-            keyboardDidChangeVisible(notification: notification)
+        if let notification = keyboardListener?.lastNotification {
+            keyboardListener?.keyboardDidChangeVisible(notification: notification)
         }
-    }
-    
-    @objc private func keyboardDidChangeVisible(notification: Notification) {
-        
-        guard let firstItem = self.firstItem as? UIView else {
-            print("NYSKEYBOARDHELPER: First item needs to be a UIView")
-            return
-        }
-        
-        guard let superview = firstItem.superview else {
-            print("NYSKEYBOARDHELPER: First item needs a superview of the type UIView")
-            return
-        }
-        
-        var animationDuration: TimeInterval = 0.0
-        var animationOptions: UIViewAnimationOptions = UIViewAnimationOptions(rawValue: 0)
-        var endFrame = CGRect.zero
-        
-        if let animationCurve = notification.userInfo?[UIKeyboardAnimationCurveUserInfoKey] as? UIViewAnimationCurve {
-            animationOptions = UIViewAnimationOptions(rawValue: UInt(animationCurve.rawValue << 16))
-        }
-        
-        if let animationDurationFromUserInfo = notification.userInfo?[UIKeyboardAnimationDurationUserInfoKey] as? TimeInterval {
-            animationDuration = animationDurationFromUserInfo
-        }
-        
-        if let endFrameFromUserInfo = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? CGRect {
-            endFrame = endFrameFromUserInfo
-        }
-        
-        let contentAbsoluteFrame = superview.convert(firstItem.frame, to: nil)  //To nil -> On the Window
-        let offset = contentAbsoluteFrame.maxY - endFrame.minY + extraIndent
-        let keyboardHeight = max(0, offset)
-        
-        animationOptions = UIViewAnimationOptions(rawValue: animationOptions.rawValue | UIViewAnimationOptions.layoutSubviews.rawValue)
-        
-        lastNotification = notification
-        
-        UIView.animate(withDuration: animationDuration, delay: 0.0, options: animationOptions, animations: {
-            self.constant = keyboardHeight
-            firstItem.layoutIfNeeded()
-        }, completion: nil)
-        
     }
 }
 
